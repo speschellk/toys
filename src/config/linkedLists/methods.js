89573a5d18e02d68ea12e02357c49d851ce92db0
Kay Christensen <kaychristensen@gmail.com>
2017-10-04 22:00:41 -0700

    Add checkForDups method.

--- src/config/linkedLists/methods.js
+++ src/config/linkedLists/methods.js
@@ -1,10 +1,29 @@
 const { LinkedList } = require('./classes');
 
-exports.buildList = function(values) {
+exports.buildList = function (values) {
   const list = new LinkedList();
 
   for (let val of values) {
     list.addToTail(val);
   }
   return list;
+}
+
+exports.checkForDups = function(list) {
+  let node = list.head;
+  let hasDups = false;
+
+  while (node && !hasDups) {
+    let target = node.value;
+    let next = node.next;
+
+    while (next && !hasDups) {
+      if (next.value === target) {
+        hasDups = true;
+      }
+      next = next.next;
+    }
+    node = node.next;
+  }
+  return hasDups;
 }
