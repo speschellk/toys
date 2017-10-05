0147ebc8cda4beb8363268692d8722c84e64c1d3
Kay Christensen <kaychristensen@gmail.com>
2017-10-04 22:02:33 -0700

    Finish tests for removeDuplicates.

--- spec/list.test.js
+++ spec/list.test.js
@@ -1,4 +1,5 @@
 const { uniqueList, dupList, spreadList, singleNodeList, emptyList } = require('../src/config/linkedLists/lists');
+const { checkForDups } = require('../src/config/linkedLists/methods');
 const { removeDuplicates } = require('../src/problems/linkedLists/removeDuplicates');
 const { deleteMiddleNode } = require('../src/problems/linkedLists/deleteMiddleNode');
 const { kthToLast, kToLast, kthToLastMap } = require('../src/problems/linkedLists/kthToLast');
@@ -6,31 +7,30 @@ const { partition } = require('../src/problems/linkedLists/partition');
 
 
 describe('Linked list tests', () => {
+  let deduped = removeDuplicates(dupList);
 
   describe('Remove duplicates', () => {
-    let dupsRemoved = removeDuplicates(dupList);
-
     test('returns null if empty list or no list passed in', () => {
       expect(removeDuplicates()).toBeNull();
     });
 
     test('list contains no duplicate node values', () => {
-      expect(dupsRemoved).toMatchObject(uniqueList);
+      expect(checkForDups(deduped)).toBeFalsy();
     });
 
     test('list retains all unique node values', () => {
-      expect(dupsRemoved).toMatchObject(uniqueList);
+      expect(deduped).toMatchObject(uniqueList);
     });
 
     test('list still has head and tail properties', () => {
-      expect(dupsRemoved.head).toBeTruthy();
-      expect(dupsRemoved.head.value).toBe(1);
-      expect(dupsRemoved.tail).toBeTruthy();
-      expect(dupsRemoved.tail.value).toBe(5);
+      expect(deduped.head).toBeTruthy();
+      expect(deduped.head.value).toBe(1);
+      expect(deduped.tail).toBeTruthy();
+      expect(deduped.tail.value).toBe(6);
     });
   });
 
-  describe('kth to lasts', () => {
+  describe('kth to last', () => {
     test('returns null if empty list or no list passed in', () => {
       expect(kthToLast()).toBeNull();
       expect(kToLast()).toBeNull();
@@ -40,32 +40,32 @@ describe('Linked list tests', () => {
 });
 // deleteMiddleNode
 /* Test cases */
-console.log(deleteMiddleNode(emptyList)); // null
-deleteMiddleNode(dupList.head.next);
-console.log(dupList); // 1, 1, 3, 1, 2, 4, 8, 8, 5;
+// console.log(deleteMiddleNode(emptyList)); // null
+// deleteMiddleNode(dupList.head.next);
+// console.log(dupList); // 1, 1, 3, 1, 2, 4, 8, 8, 5;
 
 //kthToLast
 /* Test cases */
-console.log(kthToLast());            // null
-console.log(kthToLast(list, 4));     // node.value = 3, node.next = 4
-console.log(kthToLast(list, 12));    // null
-console.log(kthToLast(list, 1));     // node.value = 6, node.next = null
-console.log(kthToLast(list, 6));     // node.value = 1, node.next = 2
-console.log(kthToLast(list2, 1));    // node.value = 1, node.next = null
+// console.log(kthToLast());            // null
+// console.log(kthToLast(list, 4));     // node.value = 3, node.next = 4
+// console.log(kthToLast(list, 12));    // null
+// console.log(kthToLast(list, 1));     // node.value = 6, node.next = null
+// console.log(kthToLast(list, 6));     // node.value = 1, node.next = 2
+// console.log(kthToLast(list2, 1));    // node.value = 1, node.next = null
 
-console.log(kToLast());              // null
-console.log(kToLast(list, 4));       // node.value = 3, node.next = 4
-console.log(kToLast(list, 12));      // null
-console.log(kToLast(list, 1));       // node.value = 6, node.next = null
-console.log(kToLast(list, 6));       // node.value = 1, node.next = 2
-console.log(kToLast(list2, 1));      // node.value = 1, node.next = null
+// console.log(kToLast());              // null
+// console.log(kToLast(list, 4));       // node.value = 3, node.next = 4
+// console.log(kToLast(list, 12));      // null
+// console.log(kToLast(list, 1));       // node.value = 6, node.next = null
+// console.log(kToLast(list, 6));       // node.value = 1, node.next = 2
+// console.log(kToLast(list2, 1));      // node.value = 1, node.next = null
 
-console.log(kthToLastMap());         // null
-console.log(kthToLastMap(list, 4));  // node.value = 3, node.next = 4
-console.log(kthToLastMap(list, 12)); // null
-console.log(kthToLastMap(list, 1));  // node.value = 6, node.next = null
-console.log(kthToLastMap(list, 6));  // node.value = 1, node.next = 2
-console.log(kthToLastMap(list2, 1)); // node.value = 1, node.next = null
+// console.log(kthToLastMap());         // null
+// console.log(kthToLastMap(list, 4));  // node.value = 3, node.next = 4
+// console.log(kthToLastMap(list, 12)); // null
+// console.log(kthToLastMap(list, 1));  // node.value = 6, node.next = null
+// console.log(kthToLastMap(list, 6));  // node.value = 1, node.next = 2
+// console.log(kthToLastMap(list2, 1)); // node.value = 1, node.next = null
 
 
 //partition
