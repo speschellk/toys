9e690d9a3b7f202aafdc33d65c83643ba6bf3b1e
Kay Christensen <kaychristensen@gmail.com>
2017-10-31 11:36:33 -0700

    Edit sumSubset.

--- src/problems/DP/sumSubset.js
+++ src/problems/DP/sumSubset.js
@@ -1,16 +1,23 @@
-/* Return a boolean indicating whether there is a non-empty subset of a given set that will add
-up to a given sum. */
+/* Return a boolean indicating whether there is a non-empty subset of a given set that will add up to a given sum. */
 
-function sumSubset(set, sum) {
+function sumSubset(set, sum) {  // O(set * sum + set + sum) --> worst case O(n^2)
   if (!set.length || sum < 0) return false;
 
-  let newVal;
-  let m = makeMemoMatrix(set, sum);
+  let val;
+  let m = makeMemoMatrix(set, sum);            // O(s + sum)
 
-  for (let row = 1; row < set.length; row++) {
-    newVal = set[row];
-    for (let col = 0; col <= sum; col++) {
-      determineCell(m, row, col, newVal);
+  // O(set * sum)
+  for (let row = 1; row < set.length; row++) { // O(s) wrt set of numbers
+    val = set[row];
+    for (let col = 0; col <= sum; col++) {     // O(sum)
+      if (val > col) {
+        m[row][col] = m[row - 1][col];
+      } else if (val === col) {
+        m[row][col] = true;
+      } else {
+        let diff = col - val;
+        m[row][col] = m[row - 1][col] || m[row - 1][diff];
+      }
     }
   }
   return m[set.length - 1][sum];
@@ -27,7 +34,7 @@ function determineCell(m, row, col, val) {
   }
 }
 
-function makeMemoMatrix(set, sum) {
+function makeMemoMatrix(set, sum) { // O(s + sum)
   let matrix = [];
   for (let row = 0; row < set.length; row++) {
     matrix.push([]);
@@ -43,7 +50,7 @@ function makeMemoMatrix(set, sum) {
 }
 
 let set = [0, 1, 2, 5, 7];
-let set2 = [0,0,0,0,0];
+let set2 = [0, 0, 0, 0, 0];
 console.log(sumSubset([])); // false
 console.log(sumSubset(set, -1)); // false
 console.log(sumSubset(set, 0)); // true
